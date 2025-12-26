12e28f911120b988be9bfbc47a7ee9f89352e80a
DESKTOP-7AS62SI\root <hvivox@hotmail.com>
2025-12-25 22:51:41 -0400

    feat: [CERTIDAOV1-1] Adiciona FeedbackPanel e formulário Adiciona um FeedbackPanel para exibir mensagens de sucesso/erro e implementa a estrutura básica do formulário de certidão.
    - Adicionado FeedbackPanel na BasePage para exibir mensagens globais.
    - Criada a estrutura do formulário em CertidaoFormPage.html com campos para número, tipo, interessado, data de emissão e status.
    - Adicionada a lógica inicial para o formulário em CertidaoFormPage.java, incluindo a criação do formulário, campos e tratamento de submissão (ainda sem persistência).

--- src/main/java/com/hvivox/certidoes/BasePage.java
+++ src/main/java/com/hvivox/certidoes/BasePage.java
@@ -5,13 +5,17 @@ import com.hvivox.certidoes.page.CertidaoListPage;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 
 public abstract class BasePage extends WebPage {
 
     public BasePage() {
         add(new Label("appName", "Certidões"));
         add(new Label("appTitle", "Certidões"));
-
+        
+        // FeedbackPanel para mensagens de sucesso/erro
+        add(new FeedbackPanel("feedback"));
+        
         // Links do menu
         add(new BookmarkablePageLink<>("linkHome", HomePage.class));
         add(new BookmarkablePageLink<>("linkListar", CertidaoListPage.class));
