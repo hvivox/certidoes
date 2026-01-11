8258bb4665c9650f9dff222c89aa366ae1992f74
DESKTOP-7AS62SI\root <hvivox@hotmail.com>
2026-01-10 23:34:38 -0400

    feat: [CERTIDOES-01] Cria estrutura de pacotes Este commit cria a estrutura de pacotes inicial para o projeto OpenSingular.
    Cria os pacotes config, packages, types, validators, views e wicket.
    Adiciona arquivos package-info.java a cada pasta para documentação.
    
    Classes/arquivos criados:
    
    src/main/java/com/hvivox/certidoes/singular/config/package-info.java
    src/main/java/com/hvivox/certidoes/singular/packages/package-info.java
    src/main/java/com/hvivox/certidoes/singular/types/package-info.java
    src/main/java/com/hvivox/certidoes/singular/validators/package-info.java
    src/main/java/com/hvivox/certidoes/singular/views/package-info.java
    src/main/java/com/hvivox/certidoes/singular/wicket/package-info.java

---
+++ src/main/java/com/hvivox/certidoes/singular/types/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Módulo 1 - Tarefa 1: Setup inicial
+ * 
+ * Pacote para definição de tipos (STypes).
+ * 
+ * Estrutura:
+ * - basic/: Tipos básicos reutilizáveis (Endereco, Pessoa, Contato)
+ * - domain/: Tipos do domínio (Certidao, Requerente, Parecer)
+ * - document/: Formulários versionados (FormularioCertidaoV1, V2)
+ * 
+ * @author OpenSingular Implementation
+ * @version 1.0
+ */
+package com.hvivox.certidoes.singular.types;
---
+++ src/main/java/com/hvivox/certidoes/singular/validators/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Módulo 3: Validação
+ * 
+ * Pacote para validadores customizados reutilizáveis.
+ * 
+ * Exemplos: CPFValidator, CNPJValidator, CEPValidator
+ * 
+ * @author OpenSingular Implementation
+ * @version 1.0
+ */
+package com.hvivox.certidoes.singular.validators;
